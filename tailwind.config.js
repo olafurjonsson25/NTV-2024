84779123f94469310db2217ee10797983375760d
brolafur <olafurjonsson52@gmail.com>
2024-03-05 17:41:32 +0000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["C:\Users\Notandi\Documents\ntv visual code\NTV-2024.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
